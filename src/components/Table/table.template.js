6ec22e1d143a3a50176ead2acfb5b2b02af8e4a8
Volodymyr Rusynov <vrusynov@gmail.com>
2020-06-11 19:52:07 +0300

    finish table resize

--- src/components/Table/table.template.js
+++ src/components/Table/table.template.js
@@ -3,15 +3,25 @@ const CODES = {
     Z: 90,
 };
 
-function toCell() {
+function toCell(_, col) {
     return `
-        <div class="cell" contenteditable></div>
+        <div class="cell" 
+             contenteditable 
+             data-col="${col}">
+        </div>
     `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
     return `
-        <div class="column">${col}</div>
+        <div class="column"
+             data-type="resizable"
+             data-col=${index}>
+            ${col}
+            <div class="col-resize" 
+                 data-resize="col">
+            </div>
+        </div>
     `;
 }
 
@@ -21,10 +31,24 @@ function toChar(_, index) {
 }
 
 function createRow(index, content) {
+    let resize = '';
+    if (index) {
+        resize = `
+            <div class="row-resize" 
+                 data-resize="row">
+            </div>
+        `;
+    }
     return `
-        <div class="row">
-            <div class="row-info">${index ? index : ''}</div>
-            <div class="row-data">${content}</div>
+        <div class="row"
+             data-type="resizable">
+            <div class="row-info">
+                ${index ? index : ''}
+                ${resize}
+            </div>
+            <div class="row-data">
+                ${content}
+            </div>
         </div>
     `;
 }
