bbd902cf69c96dbccc876c1e679b59b9c4f716d0
Volodymyr Rusynov <vrusynov@gmail.com>
2020-06-12 16:02:57 +0300

    Finish base table logic

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,7 +1,15 @@
 // pure function
+
 export function capitalize(string) {
     if (typeof string !== 'string') {
         return '';
     }
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function range(start, end) {
+    const difference = Math.abs(end - start);
+    return new Array(difference + 1)
+        .fill('')
+        .map((_, index) => Math.min(start, end) + index);
 }
